5a77c8647c93fb29766893c4d595539697d6aaaa
lorus1 <53980303+lorus1@users.noreply.github.com>
2022-02-16 17:21:33 -0600

    comentar ssl

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const fs = require('fs');
 const https = require('https');
 
 // Certificate
-const privateKey = fs.readFileSync('/home/ec2-user/ssl/private.key', 'utf8');
-const certificate = fs.readFileSync('/home/ec2-user/ssl/certificate.crt', 'utf8');
-const ca = fs.readFileSync('/home/ec2-user/ssl/ca_bundle.crt', 'utf8');
+//const privateKey = fs.readFileSync('/home/ec2-user/ssl/private.key', 'utf8');
+//const certificate = fs.readFileSync('/home/ec2-user/ssl/certificate.crt', 'utf8');
+//const ca = fs.readFileSync('/home/ec2-user/ssl/ca_bundle.crt', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca:ca
-};
+//const credentials = {
+//	key: privateKey,
+//	cert: certificate,
+//	ca:ca
+//};
 
 
 // Starting both http & https servers
-//const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+const httpServer = http.createServer(app);
+//const httpsServer = https.createServer(credentials, app);
 
-//httpServer.listen(4000, () => {
-//	console.log('HTTP Server running on port 70');
-//});
-
-
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
 });
+
+
+//httpsServer.listen(443, () => {
+//	console.log('HTTPS Server running on port 443');
+//});
