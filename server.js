67d93234d16efadb92fddaa088027886d93a4785
lorus1 <53980303+lorus1@users.noreply.github.com>
2022-03-03 16:59:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const https = require('https');
 const httpServer = http.createServer(app);
 //const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(4000, () => {
+httpServer.listen(5000, () => {
 	console.log('HTTP Server running on port 80');
 });
 
